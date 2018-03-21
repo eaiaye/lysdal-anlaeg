bc03258d4d725cc638cd30f91140e20cf4be6358
sero <sebastian.ryberg92@gmail.com>
2018-03-21 15:02:36 +0100

    test

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,14 @@
-function myFunction() {
-    alert("Who are you?")
-};
-
-function mySecondFunction() {
-    alet("Welcome, sir.")
+function myMap() {
+    var myLatLng = {lat: 56.177559, lng: 9.554179};
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: myLatLng,
+        zoom: 15
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Lysdal Anlæg, Borgergade 1, 8600 Silkeborg'
+    });
 }
